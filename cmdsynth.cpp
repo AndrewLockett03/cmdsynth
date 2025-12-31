4ce00ab8167d8e39c74e1f833b78bbc1e695cf8d
Andrew Lockett <yod7@txstate.edu>
2025-12-30 21:32:54 -0600

    Restuctured and removed duplicate code

--- cmdsynth.cpp
+++ cmdsynth.cpp
@@ -11,6 +11,8 @@
 
 int main(int argc, char** argv) {
 
+    const float GAIN = 0.316f; // Overall gain factor
+
     if (argc < 6) {
         std::cerr << "Usage: " << argv[0] << " <sample_rate> <frequency> <duration> <waveform_type> <cutoff>\n";
         return 1;
@@ -20,7 +22,7 @@ int main(int argc, char** argv) {
     char* frequencyStr = argv[2];   // Note name from command line
     float duration = atof(argv[3]);   // Duration from command line
     int waveType = atoi(argv[4]);       // Waveform type 1-4 from command line
-    float cutoff = atoi(argv[5]);   // Cutoff frequency for low-pass filter
+    float cutoff = atof(argv[5]);   // Cutoff frequency for low-pass filter
 
     int offset = 0;
     // Decode note name
@@ -65,34 +67,33 @@ int main(int argc, char** argv) {
     float frequency = 440 * pow(2, (offset + (octave - 5) * 12) / 12.0); // Calculate frequency
     std::cout << "Calculated frequency: " << frequency << " Hz\n";
 
-    LowPassFilter lpf = LowPassFilter(cutoff, sampleRate); // 16kHz cutoff
+    LowPassFilter lpf = LowPassFilter(cutoff, sampleRate);
     int totalSamples = sampleRate * duration;
-    drwav_int16* buffer = new drwav_int16[totalSamples];
+    float* buffer = new float[totalSamples];
 
     double theta = 2 * M_PI * frequency / sampleRate;
+    std::cout << "Theta: " << theta << "\n";
     float temp = 0;
 
-    //std::cout << "Generating " << type << " wave at " << frequency << " Hz for " << duration << " seconds.\n";
+    std::cout << "Generating " << waveType << " wave at " << frequency << " Hz for " << duration << " seconds.\n";
     switch(waveType) {
         case 1:
             std::cout << "Waveform: Sine Wave\n";
             for (int i = 0; i < totalSamples; ++i) {
-                buffer[i] = static_cast<drwav_int16>(sin(i * theta) * 0.316f * UINT16_MAX); // Default to sine wave
+                buffer[i] = sin(static_cast<double>(i) * theta); // Default to sine wave
             }
             break;
         case 2:
             std::cout << "Waveform: Square Wave\n";
             for (int i = 0; i < totalSamples; ++i) {
-                temp = (sin(i * theta) > 0) ? 1.0f : -1.0f; // Square wave
-                buffer[i] = static_cast<drwav_int16>(temp * 0.316f * UINT16_MAX); // Convert to 16-bit PCM
+                buffer[i] = (sin(i * theta) > 0) ? 1.0f : -1.0f; // Square wave
             }
             break;
         case 3:
             std::cout << "Waveform: Sawtooth Wave\n";
             for (int i = 0; i < totalSamples; ++i) {
                 double d_i = static_cast<double>(i);
-                temp = 2.0 * (d_i * frequency / sampleRate - floor(d_i * frequency / sampleRate)) - 1.0; // Sawtooth wave
-                buffer[i] = static_cast<drwav_int16>(temp * 0.316f * UINT16_MAX); // Convert to 16-bit PCM
+                buffer[i] = 2.0 * (d_i * frequency / sampleRate - floor(d_i * frequency / sampleRate)) - 1.0; // Sawtooth wave
             }
             break;
         case 4:
@@ -100,8 +101,7 @@ int main(int argc, char** argv) {
             for (int i = 0; i < totalSamples; ++i) {
                 double d_i = static_cast<double>(i);
                 temp = 2.0 * (d_i * frequency / sampleRate - floor(d_i * frequency / sampleRate)) - 1.0; // Sawtooth wave
-                temp = 2.0 * fabs(temp) - 1.0; // Convert to Triangle wave
-                buffer[i] = static_cast<drwav_int16>(temp * 0.316f * UINT16_MAX); // Convert to 16-bit PCM
+                buffer[i] = 2.0 * fabs(temp) - 1.0; // Convert to Triangle wave
             }
             break;
         default:
@@ -110,9 +110,11 @@ int main(int argc, char** argv) {
             break;
     }
 
-    // Apply low-pass filter to prevent aliasing
+    // Perform final DSP and formatting for WAV
+    drwav_int16* wav_buffer = new drwav_int16[totalSamples];
     for (int i = 0; i < totalSamples; ++i) {
-        buffer[i] = lpf.update(buffer[i]);
+        buffer[i] = lpf.update(buffer[i]);  // Apply low-pass filter to prevent aliasing
+        wav_buffer[i] = static_cast<drwav_int16>(buffer[i] * 32767.0f * GAIN); // Scale to 16-bit PCM & apply gain
     }
 
     std::cout << "Writing to WAV file...\n";
@@ -126,7 +128,7 @@ int main(int argc, char** argv) {
     drwav_init_file_write(&wav, "data/recording.wav", &format, NULL);
 
 
-    drwav_uint64 framesWritten = drwav_write_pcm_frames(&wav, totalSamples, buffer);
+    drwav_uint64 framesWritten = drwav_write_pcm_frames(&wav, totalSamples, wav_buffer);
     std::cout << "Wrote " << framesWritten << " frames to recording.wav\n";
     drwav_uninit(&wav);
 
